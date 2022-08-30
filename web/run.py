7b1c31e11758b44838297e769ed0de570a956d37
kim-do-hyeon <pental@kakao.com>
2022-08-30 17:26:42 +0900

    Add word list page

--- web/run.py
+++ web/run.py
@@ -1,10 +1,21 @@
-from flask import Flask, render_template
+from flask import *
+import os
 
 app = Flask(__name__)
+
+@app.route('/')
+def main_page() :
+    return redirect('index')
 
 @app.route('/index')
 def index():
     return render_template('index.html')
+
+@app.route('/word_list')
+def word_list():
+    path = "example_data"
+    file_list = os.listdir(path)
+    return render_template('word_list.html', word_title = file_list)
 
 if __name__ == '__main__' :
     app.run(debug=True)
