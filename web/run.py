78354b194e418b428ccef5da1b8f2204fb89da92
kim-do-hyeon <pental@kakao.com>
2022-08-30 18:10:31 +0900

    Modify bugs & add word view function

--- web/run.py
+++ web/run.py
@@ -1,7 +1,10 @@
 from flask import *
 import os
+import pandas as pd
+from util import *
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "ABCD"
 
 @app.route('/')
 def main_page() :
@@ -16,6 +19,28 @@ def word_list():
     path = "example_data"
     file_list = os.listdir(path)
     return render_template('word_list.html', word_title = file_list)
+
+@app.route('/word_view/<int:id>')
+def word_view(id) :
+    path = "example_data"
+    file_list = os.listdir(path)
+    file_name = file_list[id]
+    word_file_path = path + "/" + file_name
+    print(word_file_path)
+
+    file_type = check_file_type(file_name)
+
+    if file_type != "Unknown" :
+        if file_type == "csv" :
+            df = pd.read_csv(word_file_path, encoding='cp949')
+        elif file_type == "xlsx" :
+            df = pd.read_excel(word_file_path)
+        return  render_template('word_view.html', word = df, title = file_name.split(".")[0])
+
+    else :
+        flash("Unexpected Error, Please Contact Developer")
+        return redirect('/word_list')
+
 
 if __name__ == '__main__' :
     app.run(debug=True)
